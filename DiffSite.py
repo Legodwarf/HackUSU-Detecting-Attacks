1a063632cdb84d99ac2b7b239e6d57252c3e2a75
gammagecko <smcknel@gmail.com>
2025-02-28 22:09:02 -0700

    Added DiffSite.py

---
+++ DiffSite.py
@@ -0,0 +1,13 @@
+def get_base_domain(url):
+    # Remove the protocol (http, https, etc.) if present
+    if "://" in url:
+        url = url.split("://")[1]
+
+    # Split by '/' to ignore any paths, and take the first part
+    url = url.split("/")[0]
+
+    return url
+
+# Checks if a site has a different base url
+def DiffSite(url1, url2):
+    return get_base_domain(url1) != get_base_domain(url2)
