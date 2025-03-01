0633d3b102a5dfb9579edb5cb7ae350fb0d8a6a4
gammagecko <smcknel@gmail.com>
2025-02-28 22:52:42 -0700

    Added CheckHidden.py

---
+++ CheckHidden.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def has_hidden_href(url):
+    """
+    Checks if there's any <a> tag with an href that is hidden.
+    Returns True if such a tag is found; otherwise, False.
+    """
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    for tag in soup.find_all('a', href=True):  # Only consider <a> tags with an href
+        style = tag.get('style', '')
+        # Check for common hiding styles
+        if 'display: none' in style or 'visibility: hidden' in style or 'opacity: 0' in style:
+            return True
+
+    return False
